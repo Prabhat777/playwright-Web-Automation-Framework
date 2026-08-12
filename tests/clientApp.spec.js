14913ab33167b1184437e0186f3d720fba1df6cf
Prabhat777 <prabhaatsingh87@gmail.com>
2026-08-12 06:58:11 +0530

    added utils files

--- tests/clientApp.spec.js
+++ tests/clientApp.spec.js
@@ -3,26 +3,23 @@ const { LoginPage } = require('../pages/LoginPage');
 const { DashboardPage } = require('../pages/DashboardPage');
 const { CartPage } = require('../pages/CartPage');
 const { CheckoutPage } = require('../pages/CheckoutPage');
+const dataset = JSON.parse(JSON.stringify(require('../utils/placeorder.json')));
 
 test.only('Client App Login', async ({ page }) => {
-
-    const username = "prabhat.singh@gmail.com";
-    const password = "Password@12345";
-    const productName = "ZARA COAT 3";
 
     // Login Form Page 
     const loginPage = new LoginPage(page);
     await loginPage.goto();
-    await loginPage.login(username, password);
+    await loginPage.login(dataset.username, dataset.password);
 
 
     // Dashboard Page 
     const dashboardPage = new DashboardPage(page);
-    await dashboardPage.validateCartAction(productName);
+    await dashboardPage.validateCartAction(dataset.productName);
 
     // Validate in Cart Page 
     const cartPage = new CartPage(page);
-    await cartPage.validateProductOnCartPage(productName);
+    await cartPage.validateProductOnCartPage(dataset.productName);
 
     // Checkout Page 
     const checkoutPage = new CheckoutPage(page);
@@ -30,14 +27,6 @@ test.only('Client App Login', async ({ page }) => {
 
 
 })
-
-
-
-
-
-
-
-
 
 
 
